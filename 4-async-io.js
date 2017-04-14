3ac3ce6f80d0283b66c43ef2e6a61367fd3c05ab
Jordan Blakey <jordan@jordanblakey.com>
2017-04-14 12:03:17 -0500

    Quick commit.

---
+++ 4-async-io.js
@@ -0,0 +1,27 @@
+/* split of undefined error
+var fs = require('fs');
+var lines = fs.readFile(process.argv[2], 'utf8', function callback (err, data) { if (!err) {return data} else { return 'error!' } } ).split('\n').length -1;
+console.log(lines);
+*/
+
+var fs = require('fs');
+var path = process.argv[2];
+
+fs.readFile(path, 'utf8', function(err, data) {
+  var lines = data.split('\n').length - 1;
+  console.log(lines);
+});
+
+/* OFFICIAL SOLUTION
+var fs = require('fs')
+var file = process.argv[2];
+
+fs.readFile(file, function(err, contents) {
+  if (err) {
+    return console.log(err)
+  }
+  // fs.readFile(file, 'utf8', callback) can also be used
+  var lines = contents.toString().split('\n').length - 1
+  console.log(lines)
+})
+*/
